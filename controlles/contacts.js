95e7e4ca14751c32071ee9734b5a4f3e3a703d44
Ilya Kulish <ilyakulish06@outlook.com>
2023-09-03 21:04:25 +0400

    trying to connect mongo

--- controlles/contacts.js
+++ controlles/contacts.js
@@ -1,42 +1,24 @@
-const contacts = require("../models/contacts");
-const HttpError = require("../utils/HttpError");
-const ctrlWrapper = require("../utils/ctWrapper");
+const Contact = require('./models/contacts.js');
 
-const listContacts = async (req, res) => {
-  const result = await contacts.listContacts();
-  res.json(result);
-};
+async function updateStatusContact(contactId, body) {
+  try {
+    const updatedContact = await Contact.findByIdAndUpdate(
+      contactId,
+      { favorite: body.favorite },
+      { new: true }
+    );
 
-const getContactById = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
-  if (!result) throw HttpError(404, "Not found");
-  res.json(result);
-};
+    if (!updatedContact) {
+      return null;
+    }
 
-const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
-  res.status(201).json(result);
-};
-
-const removeContact = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
-  if (!result) throw HttpError(404, "Not found");
-  res.json({ message: "contact deleted" });
-};
-
-const updateContact = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
-  if (!result) throw HttpError(404, "Not found");
-  res.json(result);
-};
+    return updatedContact;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
 
 module.exports = {
-  listContacts: ctrlWrapper(listContacts),
-  getContactById: ctrlWrapper(getContactById),
-  addContact: ctrlWrapper(addContact),
-  removeContact: ctrlWrapper(removeContact),
-  updateContact: ctrlWrapper(updateContact),
+  // другие функции
+  updateStatusContact,
 };
